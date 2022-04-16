e66a710e8d01c4bdaf44b3a5bcaa05660c5830ae
nurmdrafi <nurmdrafi2014@gmail.com>
2022-04-17 00:58:01 +0600

    initial router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Checkout from './components/Checkout/Checkout';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import NotFound from './components/NotFound/NotFound';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/checkout" element={<Checkout></Checkout>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
