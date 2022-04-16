7661759d190535f253e2ade3540b1925672bc77e
nurmdrafi <nurmdrafi2014@gmail.com>
2022-04-17 01:30:35 +0600

    setup custom link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    );
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Rock Fitness</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <CustomLink to="/home">Home</CustomLink>
+            <CustomLink to="/blog">Blog</CustomLink>
+            <CustomLink to="/checkout">Checkout</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
+            <Link to="/login"><button className="btn btn-dark">Log In</button></Link>
+            <button className="btn btn-dark">Log Out</button>
+            {/* {
+                user ? <button>Log Out</button> : <button>Log In</button>
+            } */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
